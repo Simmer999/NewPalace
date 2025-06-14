44fa037f216da68355e043731bcae7190588365e
Sim <fearlessleader5900@gmail.com>
2025-06-13 20:56:53 -0600

    simplified app.js and server.js

--- server.js
+++ server.js
@@ -1,33 +1,5 @@
 const http = require('http')
-// const app = require('./app')
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-const path = require('path')
-const flash = require('connect-flash')
-const session = require('express-session')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const THREE = require('three')
-
-app.set('view engine', 'ejs')
-app.set('views', (path.join(__dirname, 'views')))
-
-app.use(express.urlencoded({ extended: false }))
-app.use(flash())
-app.use(session({
-    secret : 'secret',
-    resave : true,
-    saveUninitialized : true
-}))
-app.use(bodyParser.json())
-
-app.use(express.static(__dirname + '/'));
-
-app.get("/", (req, res) =>{
-    res.render("home")
-})
+const app = require('./app')
 
 
 const server = http.createServer(app)

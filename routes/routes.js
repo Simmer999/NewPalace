8633ba676fb41b7a90e65a536343bd79efde0bf1
Sim <fearlessleader5900@gmail.com>
2025-06-16 22:19:41 -0600

    coloured the scrollbar

--- routes/routes.js
+++ routes/routes.js
@@ -5,14 +5,8 @@ const routes = express.Router()
 routes.get("/index", (req, res) =>{
     res.render("index")
 })
-routes.get("/collections", (req, res) =>{
-    res.render("collections")
-})
 routes.get('/books', (req, res) => {
     res.render('books')
-})
-routes.get('/bookDetails', (req, res) => {
-    res.render('bookDetails')
 })
 routes.get('/poems', (req, res) => {
     res.render('poems')

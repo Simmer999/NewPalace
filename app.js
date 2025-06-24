a4818fd22fa6bf0bccc2c59b518ef8aa253a57dc
Sim <fearlessleader5900@gmail.com>
2025-06-23 20:27:29 -0600

    still working on the PATCH route

--- app.js
+++ app.js
@@ -4,21 +4,10 @@ const app = express()
 const route = express.Router()
 const ejs = require('ejs')
 const path = require('path')
+const morgan = require('morgan');
 const bodyParser = require('body-parser')
 
-
-// const mongoose = require('mongoose')
-// mongoose.set('strictQuery', true);
-// mongoose.connect(process.env.DB, {
-//     useNewUrlParser: true, 
-//     useUnifiedTopology : true
-// })
-// const db = mongoose.connection
-// db.on('error', console.log.bind(console, 'connection error'))
-// db.once('open', (callback) => {
-//     console.log('Connected to MongoDB #1.')//----------------------
-// })
-
+app.use(morgan('tiny'));
 app.use('/', require('./routes/mongoose'))
 app.use(express.static(__dirname + '/'));
 app.use('/', require('./routes/routes'))
@@ -29,6 +18,7 @@ app.use('/', require('./routes/movies'))
 
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 
 app.set('view engine', 'ejs')
 app.set('views', (path.join(__dirname, 'views')))
